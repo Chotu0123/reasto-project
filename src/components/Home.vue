129316a97d9a6dd7cdbc358129dfdf6bb8f78fc6
Nitesh web <ns9086118@gmail.com>
2021-08-03 10:29:52 +0530

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1> hello user, Welcome on Home Page</h1>
+</template>
+
+<script>
+    export default{
+        name:'Home',
+        mounted()
+            {
+        let user= localStorage.getItem('user-info');
+        if(!user)
+        {
+        this.$router.push({name:'Home'})
+        }
+    }
+        }
+    </script>
